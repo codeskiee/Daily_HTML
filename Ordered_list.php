2f9714d596dac4710b317cb5fd657381cb3f33d2
codeskiee <code.skiee99@gmail.com>
2024-07-09 15:44:34 +0700

    Ordered_list.php

---
+++ Ordered_list.php
@@ -0,0 +1,31 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <title> Ordered List </title>
+    </head>
+    <body>
+        <h1> Selamat Datang Dunia HTML ! </h1>
+            <p> Akhirnya saya bisa belajar HTML. </p>
+            <p> Dengan bantuan buku <b><i>"HTML dan CSS"</i></b>
+                Karangan Jubilee Enterprise (diterbitkan oleh PT ELex Media Komputindo), belajar HTML menjadi sangat mudah dan sederhana.</p>
+            <br>
+        <h2> Langkah-langkah Membeli Buku </h2>
+            <ol style="list-style-type : decimal">
+                <li>Masuklah ke toko buku Gramedia
+                <li> Sebutkan identitas buku berikut :
+              <ul style="list-style-type : disc">
+                <li><b> Judul </b>     : HTML dan CSS
+                <li><b> Penerbit </b>  : PT Elex Media Komputindo
+                <li><b> Penulis </b>   : Jubilee Enterprise
+              </ul>
+                <li> Pergilah ke kasir.
+                <li> Bayarlah buku tersebut
+            </ol>
+    </body>
+</html>
+  <!-- Catatan -->
+       <!-- style="list-style-type : ****" Sebagai rumus menggunakan Atribut tersebut
+        <ol style="list-style-type : decimal">
+            <li>Masuklah ke toko buku Gramedia
+            <li> Sebutkan identitas buku berikut :
+        </ol>   INI UNTUK CONTOH Ordered List -->
